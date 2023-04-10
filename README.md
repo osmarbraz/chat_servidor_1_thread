cd5430d8f278836d1526ca778a90474772fb1a6e
Osmar de Oliveira Braz Junior <osmarbraz@hotmail.com>
2023-04-10 16:35:15 -0300

    Início do projeto

---
+++ README.md
@@ -0,0 +1,4 @@
+# Servidor de Chat usando 1 Thread e Java Socket.
+
+- Programa servidor java socket de chat com 1 thread.
+- Permite receber a conexão de um único cliente.
---
+++ src/main/java/Principal.java
@@ -0,0 +1,27 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        try {
+            ServerSocket servidor = new ServerSocket(4444);
+            System.out.println("Aguardando cliente!");
+            System.out.println("Servidor :" + servidor.getInetAddress().getHostAddress());
+            Socket socket = servidor.accept(); // espera	  
+            System.out.println("Chegou o cliente :" + socket.getRemoteSocketAddress());
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            String mensagem = "";
+            while ((mensagem = in.readLine()) != null) {
+                System.out.println(mensagem);
+            }
+            System.out.println("Servidor finalizado!");
+        } catch (IOException e) {
+            System.err.println("Problemas de IO");
+        }
+    }
+}
